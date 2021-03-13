c06d17b4611f28188994dcf96097db51052e04e6
John Dunstan <johntdunstan@gmail.com>
2021-03-13 12:32:30 -0700

    Week 2 Assignment - Add a comment

--- redux/comments.js
+++ redux/comments.js
@@ -5,6 +5,14 @@ export const comments = (state = { errMess: null, comments: [] }, action) => {
     case ActionTypes.ADD_COMMENTS:
       return { ...state, errMess: null, comments: action.payload }
 
+    case ActionTypes.ADD_COMMENT:
+      const newComment = { ...action.payload, id: state.comments.length }
+      return {
+        ...state,
+        errMess: null,
+        comments: [...state.comments, newComment],
+      }
+
     case ActionTypes.COMMENTS_FAILED:
       return { ...state, errMess: action.payload }
 
