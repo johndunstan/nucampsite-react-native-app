815c43f39fd370721661a07f964361f618a00ac7
John Dunstan <johntdunstan@gmail.com>
2021-03-16 14:52:27 -0600

    Add rubberband animation to compsite-info components

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -34,10 +34,19 @@ const mapDispatchToProps = {
 function RenderCampsite(props) {
   const { campsite } = props
 
+  const view = React.createRef()
+
   const recongnizeDrag = ({ dx }) => (dx < -200 ? true : false)
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      view.current
+        .rubberBand(1000)
+        .then((endState) =>
+          console.log(endState.finished ? 'finished' : 'canceled')
+        )
+    },
     onPanResponderEnd: (e, gestureState) => {
       console.log('pan responder end', gestureState)
       if (recongnizeDrag(gestureState)) {
@@ -71,6 +80,7 @@ function RenderCampsite(props) {
         animation="fadeInDown"
         duration={2000}
         delay={1000}
+        ref={view}
         {...panResponder.panHandlers}
       >
         <Card
